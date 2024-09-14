07d45bb708b61659224b643b66362af8912e6c51
samarjeet-singh-kheda <samarjeetsingh259@gmail.com>
2024-09-14 21:36:44 +0530

    minor fix (resolved directory path)

--- server.js
+++ server.js
@@ -24,10 +24,10 @@ app.use("/api/v1/admin", require("./routes/adminRoutes"));
 app.use("/api/v1/doctor", require("./routes/doctorRoutes"));
 
 //static files
-app.use(express.static(path.join(__dirname, "/client/build")));
+app.use(express.static(path.join(__dirname, "/build")));
 
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./build/index.html"));
 });
 
 //port
